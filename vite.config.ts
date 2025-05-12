d73e6614418d13111f0ba00f366aad0d9681604b
Martin Borg <borg.martin@outlook.com>
2025-05-12 14:08:49 +0200

    Update Vite configuration to set base path to current directory and adjust build output settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  // ...
-  base: "/",
+  base: "./",
   build: {
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
