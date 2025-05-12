3901f5a8be86b74f6340f751dc4bd7ee8e36253a
Martin Borg <borg.martin@outlook.com>
2025-05-12 13:34:01 +0200

    Enhance build process and update dependencies; add node types and adjust Vercel configuration for static builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   // ...
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        productPage: path.resolve(__dirname, "pages/productPage.html"),
+        yourcart: path.resolve(__dirname, "pages/yourcart.html"),
+        checkout: path.resolve(__dirname, "pages/checkout.html"),
+      },
+    },
+  },
 });
